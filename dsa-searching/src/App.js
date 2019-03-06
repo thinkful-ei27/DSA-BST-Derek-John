e7d578a3c1da4b0412454e65fda7e5912f5bcaf3
Derek Houck <derek@derekhouck.com>
2019-03-06 09:21:41 -0800

    Implement binary search

--- dsa-searching/src/App.js
+++ dsa-searching/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import dataset from './dataset';
 
-// const initialstate = dataset;
+const sortedData = [...dataset];
+sortedData.sort((a, b) => a - b);
 
 class App extends Component {
   constructor(props) {
@@ -13,12 +14,13 @@ class App extends Component {
     }
     this.input = React.createRef()
   }
+
   linearSearch() {
     const num = Number(this.input.current.value);
     this.setState({ tries: 0 });
     let counter = 1;
     for (let i = 0; i < dataset.length; i++) {
-      if (dataset[i] == num) {
+      if (dataset[i] === num) {
         this.setState({ tries: counter, found: true });
         return;
       }
@@ -26,13 +28,34 @@ class App extends Component {
     }
     this.setState({ tries: dataset.length, found: false });
   }
+
+  binarySearch(array, num, start = 0, end = array.length, counter = 1) {
+    if (start > end) {
+      this.setState({ tries: array.length, found: false });
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    if (item === num) {
+      this.setState({ tries: counter, found: true });
+      return;
+    }
+    else if (item < num) {
+      return this.binarySearch(array, num, index + 1, end, counter + 1);
+    }
+    else if (item > num) {
+      return this.binarySearch(array, num, start, index - 1, counter + 1);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <label>Number</label>
         <input type='number' ref={this.input}></input>
         <button className='linear-search' onClick={() => this.linearSearch()}>Linear</button>
-        <button className='binary-search'>Binary</button>
+        <button className='binary-search' onClick={() => this.binarySearch(sortedData, Number(this.input.current.value))}>Binary</button>
         <div className='results'>
           {!(this.state.found) ? 'Not in dataset.' : ''} The number of tries was {this.state.tries}
         </div>
